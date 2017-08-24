ff154bbb79266e64dd577a556ea94583b4297dce
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2017-08-24 16:44:30 +0900

    :recycle: Add frozen_string_literal: true

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require './game.rb'
 require './board.rb'
 require './mark.rb'
--- mark.rb
+++ mark.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Mark
   attr_accessor :char, :row, :col
 
